d494d39382f9c84161f936d1b047d2e5231d3133
AbolfazlRamezanian <abolfazl30not30@gmail.com>
2023-11-01 21:43:57 +0330

    update and fix

--- src/redux/api/loginSlice.js
+++ src/redux/api/loginSlice.js
@@ -5,7 +5,7 @@ import queryString from 'query-string'
 let base64encodedData = Buffer.from( "client1"+ ':' +"myClientSecretValue" ).toString('base64');
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://authorization-server:8080',
+    baseUrl: 'http://194.33.125.112:30835',
     prepareHeaders: (headers) => {
         headers.set("Authorization", "Basic " + base64encodedData)
         return headers
