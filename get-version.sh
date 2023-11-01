e8877a6dfe8f84e2fe14dbc6c271a4dddeb3ff9d
MatinVahediyan <matinvahediyan@gmail.com>
2023-11-01 17:06:28 +0000

    Update get-version.sh

--- get-version.sh
+++ get-version.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat pom.xml | grep -m 2 '<version>' | awk -F '>' '{print $2}' | awk -F '<' '{print $1}' | awk 'NR==2' > app-version
+cat package.json | grep 'version' | awk -F ': ' '{print $2}' | awk -F '"' '{print $2}' > app-version
