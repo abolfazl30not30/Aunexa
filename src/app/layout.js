c1c188faf93a1a7b0a7e5a02c2fee3266c29e10a
AbolfazlRamezanian <abolfazl30not30@gmail.com>
2023-11-02 16:58:45 +0330

    update and fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,32 +9,32 @@ import localFont from 'next/font/local'
 const fonts = localFont({
     src: [
         {
-            path: './fonts/IRANYekanLight.ttf',
+            path: '/fonts/IRANYekanLight.ttf',
             weight: '300',
             style: 'normal',
         },
         {
-            path: './fonts/IRANYekanRegular.ttf',
+            path: '/fonts/IRANYekanRegular.ttf',
             weight: '400',
             style: 'normal',
         },
         {
-            path: './fonts/IRANYekanMedium.ttf',
+            path: '/fonts/IRANYekanMedium.ttf',
             weight: '500',
             style: 'normal',
         },
         {
-            path: './fonts/IRANYekanBold.ttf',
+            path: '/fonts/IRANYekanBold.ttf',
             weight: '700',
             style: 'normal',
         },
         {
-            path: './fonts/IRANYekanExtraBold.ttf',
+            path: '/fonts/IRANYekanExtraBold.ttf',
             weight: '800',
             style: 'normal',
         },
         {
-            path: './fonts/IRANYekanBlack.ttf',
+            path: '/fonts/IRANYekanBlack.ttf',
             weight: '900',
             style: 'normal',
         },
