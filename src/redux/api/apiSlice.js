44a9be2ac6b7b8a28e4736adac79c55c10a737b6
HasanSharghi <hasansharghi94@gmail.com>
2023-11-06 12:05:38 +0330

    change api url

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,55 +1,72 @@
-'use client'
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import {logOut,setAccessToken} from './authSlice'
+"use client";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logOut, setAccessToken } from "./authSlice";
 import axios from "axios";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:9191/api/v1/',
-    prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.accessToken
-        if (token) {
-            headers.set("Authorization", `Bearer ${token}`)
-        }
-        return headers
+  baseUrl: "http://gateway.vipsoftware1.com/api/v1/",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.accessToken;
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
     }
-})
+    return headers;
+  },
+});
 
-const login =  async () =>{
-    let base64encodedData = Buffer.from( "client1"+ ':' +"myClientSecretValue" ).toString('base64');
+const login = async () => {
+  let base64encodedData = Buffer.from(
+    "client1" + ":" + "myClientSecretValue"
+  ).toString("base64");
 
-    const formData = {
-        client_id:"client1",
-        refresh_token:window.sessionStorage.getItem("refresh_token"),
-        grant_type:"refresh_token",
+  const formData = {
+    client_id: "client1",
+    refresh_token: window.sessionStorage.getItem("refresh_token"),
+    grant_type: "refresh_token",
+  };
+
+  return await axios.post(
+    "http://auth.vipsoftware1.com/oauth2/token",
+    formData,
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + base64encodedData,
+      },
     }
-
-    return await axios.post("http://localhost:8080/oauth2/token", formData, {
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": "Basic " + base64encodedData
-        }
-    })
-}
+  );
+};
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions)
+  let result = await baseQuery(args, api, extraOptions);
 
-    if (result?.error?.status === 401 || result?.error?.status === 500 ) {
-        const refreshResult = await login()
-        api.dispatch(setAccessToken(refreshResult?.data?.access_token))
-        if (refreshResult?.data) {
-            result = await baseQuery(args, api, extraOptions)
-        } else {
-            api.dispatch(logOut())
-        }
+  if (result?.error?.status === 401 || result?.error?.status === 500) {
+    const refreshResult = await login();
+    api.dispatch(setAccessToken(refreshResult?.data?.access_token));
+    if (refreshResult?.data) {
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
     }
+  }
 
-    return result
-}
+  return result;
+};
 
 export const apiSlice = createApi({
-    reducerPath:"api",
-    baseQuery: baseQueryWithReauth,
-    tagTypes: ['purchase-request','primary-store-input','primary-store-output','product-store-input','product-store-output','equipment-store-input','equipment-store-output','category','vehicle-machine','product'],
-    endpoints: builder => ({})
-})
+  reducerPath: "api",
+  baseQuery: baseQueryWithReauth,
+  tagTypes: [
+    "purchase-request",
+    "primary-store-input",
+    "primary-store-output",
+    "product-store-input",
+    "product-store-output",
+    "equipment-store-input",
+    "equipment-store-output",
+    "category",
+    "vehicle-machine",
+    "product",
+  ],
+  endpoints: (builder) => ({}),
+});
--- src/redux/api/loginSlice.js
+++ src/redux/api/loginSlice.js
@@ -1,29 +1,31 @@
-'use client'
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import queryString from 'query-string'
+"use client";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import queryString from "query-string";
 
-let base64encodedData = Buffer.from( "client1"+ ':' +"myClientSecretValue" ).toString('base64');
+let base64encodedData = Buffer.from(
+  "client1" + ":" + "myClientSecretValue"
+).toString("base64");
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:8080',
-    prepareHeaders: (headers) => {
-        headers.set("Authorization", "Basic " + base64encodedData)
-        return headers
-    }
-})
+  baseUrl: "http://auth.vipsoftware1.com",
+  prepareHeaders: (headers) => {
+    headers.set("Authorization", "Basic " + base64encodedData);
+    return headers;
+  },
+});
 
 export const loginSlice = createApi({
-    reducerPath: 'loginApi',
-    baseQuery: baseQuery,
-    endpoints: builder => ({
-        login: builder.mutation({
-            query: credentials => ({
-                url: '/oauth2/token',
-                method: 'POST',
-                body: new URLSearchParams(credentials)
-            })
-        }),
-    })
-})
+  reducerPath: "loginApi",
+  baseQuery: baseQuery,
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/oauth2/token",
+        method: "POST",
+        body: new URLSearchParams(credentials),
+      }),
+    }),
+  }),
+});
 
-export const {useLoginMutation} = loginSlice
+export const { useLoginMutation } = loginSlice;
