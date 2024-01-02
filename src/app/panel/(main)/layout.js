a2ab88acd2f29da9e8fc4627e8e7831171bc45df
AbolfazlRamezanian <abolfazl30not30@gmail.com>
2024-01-02 10:11:09 +0330

    add new pages

--- src/app/panel/(main)/layout.js
+++ src/app/panel/(main)/layout.js
@@ -1961,40 +1961,9 @@ export default function RootLayout({ children }) {
             </div>
           </div>
           <div className="mt-4 mx-1 md:m-5 h-screen w-full md:w-[70%] lg:w-[85%]">
-            {pathname === "/panel" && (
-              <div className="flex justify-between items-center text-[0.9rem] bg-white py-6 px-5 md:px-10">
-                <div className="">
-                  <h2 className="font-[800] text-[0.9rem] md:text-[1.1rem]">
-                    انتقال به داشبورد پیشرفته
-                  </h2>
-                </div>
-
-                <div className="">
-                  <Link
-                    href="/dashboard"
-                    className="flex  text-white items-center bg-mainRed border px-3 py-2 rounded"
-                  >
-                    <span className="inline text-[0.9rem]">
-                      داشبورد پیشرفته
-                    </span>
-                  </Link>
-                </div>
-              </div>
-            )}
-            {pathname === "/panel" && (
-              <div className="mt-4">
-                <CacheProvider value={cacheRtl}>
-                  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-                </CacheProvider>
-              </div>
-            )}
-            {pathname !== "/panel" && (
-              <div className="">
-                <CacheProvider value={cacheRtl}>
-                  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-                </CacheProvider>
-              </div>
-            )}
+              <CacheProvider value={cacheRtl}>
+                <ThemeProvider theme={theme}>{children}</ThemeProvider>
+              </CacheProvider>
           </div>
         </div>
       </div>
--- src/app/panel/(main)/page.js
+++ src/app/panel/(main)/page.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import React from "react";
 
 export default function Dashboard() {
     return (
         <>
+            <div className="flex justify-between items-center text-[0.9rem] bg-white py-6 px-5 md:px-10">
+                <div className="">
+                    <h2 className="font-[800] text-[0.9rem] md:text-[1.1rem]">
+                        داشبورد
+                    </h2>
+                </div>
+                <div className="">
+                    <Link
+                        href="/dashboard"
+                        className="flex  text-white items-center bg-mainRed border px-3 py-2 rounded"
+                    >
+                    <span className="inline text-[0.9rem]">
+                      داشبورد پیشرفته
+                    </span>
+                    </Link>
+                </div>
+            </div>
         </>
     )
 }
